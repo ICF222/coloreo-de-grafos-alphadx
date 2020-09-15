508123c9b78507c259a874ad0a57d943e23698e4
Alexander Espina-Leyton <al.espina@uandresbello.edu>
2020-09-15 15:35:53 -0300

    Se genera el orden de salida de los nodos

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string> 
 #include <vector>
 #include <unistd.h>
+#include <unordered_set>
 
 using namespace std;
 
@@ -25,8 +26,12 @@ vector<string> split(const string& str, const string& delim)
 // ./solver -i ./Instancias/ejemplo1.in -o ./Salidas/ejemplo1.out -t 2
 
 int main(int argc, char* argv[]) {
-  string filenameIN, filenameOUT;
-  int c, segundos;
+  string filenameIN, filenameOUT, linea;
+  int c, segundos, i;
+  vector<string> lineaSpliteada; //["e","3","2"]
+  vector<vector<string>> todosLosArcos; //[["e","3","2"],["e","4","6"]]
+  vector<int> ordenNodos;
+  unordered_set<int> listaNodos;
   
   extern char *optarg;
   opterr = 0;
@@ -52,6 +57,8 @@ int main(int argc, char* argv[]) {
 	if (!fileIn.is_open()) {
 		fileIn.open(filenameIN, ios::in);
 	}
+
+	
   
   //Archivo de Salida
   ofstream fileOut(filenameOUT);
@@ -59,7 +66,35 @@ int main(int argc, char* argv[]) {
 	if (!fileOut.is_open()) {
 		fileOut.open(filenameOUT);
 	}
+	
+	while(getline(fileIn, linea)){
+		lineaSpliteada = split(linea," ");
+		if(lineaSpliteada[0] == "e"){
+			todosLosArcos.push_back(lineaSpliteada);
+		}
+	}
+	for(i=0; i < todosLosArcos.size(); i++){
+		listaNodos.insert(stoi(todosLosArcos[i][1]));
+	}
 
+        for(i=0; i < todosLosArcos.size(); i++){
+                listaNodos.insert(stoi(todosLosArcos[i][2]));
+        }
+
+	for(auto iter = listaNodos.begin(); iter != listaNodos.end(); iter++){
+		ordenNodos.insert(ordenNodos.begin(),*iter); 
+	}
+	
+	for(i=0; i < ordenNodos.size(); i++){
+                cout << ordenNodos[i] << " ";
+        }
+	cout << endl;
+
+
+//e 1 2
+//e 2 3
+//e 4 5
+//1 2 4 3 5
 
   fileOut.close();
   
